7a816c15d482879c3771b450a9a0a4a94876f999
ronaldomb98 <ronaldomb98@gmail.com>
2017-03-17 09:08:14 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { IPayU_response } from './PayU_response';
+import { payuRespons } from './payuResponse';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+      private Payu_response: IPayU_response;
+    constructor(
+        private activatedRoute: ActivatedRoute
+    ) { }
+
+    public ngOnInit() {
+        this.init_Payu_response();
+        this.get_query_params_from_url();
+    }
+
+
+    private get_query_params_from_url(): void {
+        this.activatedRoute.queryParams.subscribe(
+            (params: Params) => {
+                this.Payu_response = {
+                    description: params[payuRespons.description],
+                    referenceCode: params[payuRespons.referenceCode],
+                    TX_VALUE: params[payuRespons.TX_VALUE],
+                    currency: params[payuRespons.currency],
+                    processingDate: params[payuRespons.processingDate],
+                    lapPaymentMethod: params[payuRespons.lapPaymentMethod],
+                    transactionId: params[payuRespons.transactionId]
+                };
+            }
+        );
+    }
+
+    private init_Payu_response(): void {
+        this.Payu_response = {
+            description: '',
+            referenceCode: '',
+            TX_VALUE: 0,
+            currency: '',
+            processingDate: '',
+            lapPaymentMethod: '',
+        };
+    }
 }
